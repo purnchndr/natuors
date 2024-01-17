2095c2656a2a2add4fe1b3c928f3104a530d9c58
Pratap <pratap.careers@gmail.com>
2024-01-17 23:41:35 +0530

    added v1 get of tours

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
+const { json } = require('express');
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
+app.use(express.json());
 const PORT = 3000;
 
-app.get('/', (req, res, next) => {
-  res.status(200).json({ message: 'Hello from server!' });
+const tours = require('./dev-data/data/tours-simple.json');
+
+// const tours = JSON.parse(
+//   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+// );
+
+app.get('/api/v1/tours', (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: { tours: tours },
+  });
+});
+
+app.post('/api/v1/tours', (req, res, next) => {
+  console.log(req.body);
+  res.send('');
 });
 
 app.listen(PORT, () => console.log('App is running'));
